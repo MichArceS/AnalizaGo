3b48beec166f33bc58d71de380439fd03a25f3d0
Josue Tomala <jotopozo@espol.edu.ec>
2021-06-21 00:09:58 -0500

    Sprint 1

--- analizadorLexico.py
+++ analizadorLexico.py
@@ -1,30 +1,29 @@
 import ply.lex as lex
 
-#lista de tokens
-tokens = ("NUMERO", "MAS", "MENOS", "MULTIPLICADOR", "DIVISOR", "MOD_DIVISION", "PARENT_IZQ","PARENT_DER",
-           "CORCHETE_IZQ", "CORCHETE_DER", "LLAVE_IZQ", "LLAVE_DER", "PUNTO", "DOS_PUNTOS", "COMA","COMILLA",
-           "DOBLE_COMILLA","COMMENT_BLOQUE_IZQ", "COMMENT_BLOQUE_DER", "COMMENT_LINEA", "FIN_SENTENCIA", "ID", "ASIGNACION",
-           "ESPACIADO", "CADENA", "MENOR", "MAYOR", "IGUALDAD", "VACIO", "MENOR_IGU", "MAYOR_IGU")
-print(len(tokens)) #31
+# lista de tokens
+tokens = ("NUMERO", "CADENA", "MAS", "MENOS", "MULTIPLICADOR", "DIVISOR", "MOD_DIVISION", "PARENT_IZQ", "PARENT_DER",
+          "CORCHETE_IZQ", "CORCHETE_DER", "LLAVE_IZQ", "LLAVE_DER", "PUNTO", "DOS_PUNTOS", "COMA", "COMILLA",
+          "DOBLE_COMILLA", "COMMENT_BLOQUE_IZQ", "COMMENT_BLOQUE_DER", "COMMENT_LINEA", "FIN_SENTENCIA", "ID", "ASIGNACION",
+          "ESPACIADO", "MENOR", "MAYOR", "IGUALDAD", "VACIO", "MENOR_IGU", "MAYOR_IGU", "BOOLEAN", "DESIGUALDAD", "INCREMENTO", 
+          "DECREMENTO", "PIPE", "AMPERSAND")
+print(len(tokens))  # 31
 
-#lista de palabras reservadas
+# lista de palabras reservadas
 reservada = {
-    'if': 'IF',
-    'then': 'THEN',
-    'else': 'ELSE',
-    'switch': 'SWITCH',
-    'case': 'CASE',
-    'while': 'WHILE',
     'break': 'BREAK',
-    'continue': 'CONTINUE',
-    'defer': 'DEFER',
-    'select': 'SELECT',
+    'default': 'DEFAULT',
     'func': 'FUNC',
     'interface': 'INTERFACE',
+    'select': 'SELECT',
+    'case': 'CASE',
+    'defer': 'DEFER',
     'go': 'GO',
+    'if': 'IF',
+    'else': 'ELSE',
+    'switch': 'SWITCH',
+    'continue': 'CONTINUE',
     'map': 'MAP',
     'struct': 'STRUCT',
-    'chan': 'CHAN',
     'const': 'CONST',
     'package': 'Package',
     'fallthrough': 'FALLTHROUGH',
@@ -33,31 +32,20 @@ reservada = {
     'for': 'FOR',
     'import': 'IMPORT',
     'return': 'RETURN',
-    'var': 'VAR',
-    'fmt': 'FMT',
-    'io': 'IO',
-    'log': 'LOG',
-    'os': 'OS',
-    'make': 'MAKE',
-    'main': 'MAIN',
-    'int': 'INT',
-    'float64': 'FLOAT64',
-    'string': 'STRING',
-    'true': 'TRUE',
-    'false': 'FALSE'
+    'var': 'VAR'
 }
-print(len(reservada)) #30
+print(len(reservada))  # 30
 
 tokens = tokens + tuple(reservada.values())
-print(len(tokens)) #61
+print(len(tokens))  # 61
 
-#expresiones regulares para tokens simples
+# expresiones regulares para tokens simples
 t_ignore = ' \t'
 t_MAS = r'\+'
-t_MENOS = r'-'
+t_MENOS = r'\-'
 t_MULTIPLICADOR = r'\*'
-t_DIVISOR = r'/'
-t_MOD_DIVISION = r'%'
+t_DIVISOR = r'\/'
+t_MOD_DIVISION = r'\%'
 t_PARENT_IZQ = r'\('
 t_PARENT_DER = r'\)'
 t_CORCHETE_IZQ = r'\['
@@ -65,9 +53,9 @@ t_CORCHETE_DER = r'\]'
 t_LLAVE_IZQ = r'\{'
 t_LLAVE_DER = r'\}'
 t_PUNTO = r'\.'
-t_DOS_PUNTOS = r':'
-t_VACIO = r'_'
-t_ASIGNACION = r':='
+t_DOS_PUNTOS = r'\:'
+t_VACIO = r'\_'
+t_ASIGNACION = r'\:?\='
 t_IGUALDAD = r'=='
 t_FIN_SENTENCIA = r';'
 t_COMA = r','
@@ -77,60 +65,83 @@ t_MENOR = r'\<'
 t_MAYOR = r'\>'
 t_MENOR_IGU = r'\<='
 t_MAYOR_IGU = r'\>='
+t_DESIGUALDAD = r'!='
+t_INCREMENTO = r'\+{2}'
+t_DECREMENTO = r'\-{2}'
+t_PIPE = r'\|'
+t_AMPERSAND = '\&'
+# funcion para reconocer identificadores
 
-#funcion para reconocer identificadores
+
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reservada.get(t.value, 'ID')
     return t
 
-#funcion para reconocer comentarios y no tomarlos en cuenta
+# funcion para reconocer comentarios y no tomarlos en cuenta
+
+
 def t_COMMENT(t):
-    r'(\#.*)|(\/\*.*\*\/)'
+    r'(\/\/.*)|(\/\*.*\*\/)'
     pass
 
-#función para reconocer número
+# función para reconocer número
+
+
 def t_NUMERO(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-#función para reconocer cadena
+# función para reconocer cadena
+
+
 def t_CADENA(t):
     r'(\'.*\')|(".*")'
     return t
 
-#función para reconocer boolean
+# función para reconocer boolean
+
+
 def t_BOOLEAN(t):
     r'true|false'
-    return t;
+    return t
 
-#función para reconocer flotantes
+
+# función para reconocer flotantes
 def t_FLOAT(t):
     r'[0-9]+\.[0-9]+'
     return t
 
-#función para conocer el número de linea donde me encuentro
+# función para conocer el número de linea donde me encuentro
+
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-#función para manejar errores en los tokens
+# función para manejar errores en los tokens
+
+
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-#función para obtener tokens
+# función para obtener tokens
+
+
 def getTokens(lexer):
     while True:
         tok = lexer.token()
         if not tok:
             break  # No more input
         print(tok)
+
+
 # Build the lexer
 lexer = lex.lex()
-linea=" "
-while linea!="":
-    linea=input(">>")
+linea = " "
+while linea != "":
+    linea = input(">>")
     lexer.input(linea)
     getTokens(lexer)
